02cce154255335be2001216a37ea6925ea1b6a0a
Germán Asencio <germanasencioanton@gmail.com>
2020-02-28 23:45:49 +0100

    JavaDoc pruebaUno

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -2,13 +2,25 @@ package sumador;
 
 public class ASumar {
 
+	/**
+	 * String en el que realizaremos las modificaciones del valor hasta obtener el resultado final
+	 */
 	private String valor_inicial;
 
+	
+	/**
+	 * Constructor en el que creamos un objeto ASumar con un valor inicial de String
+	 * @param string Número elegido 
+	 */
 	public ASumar(String string) {
 		
 		this.valor_inicial = string;
 	}
 
+	/**
+	 * Muestra el resultado final del sumatorio de las cifras del número pasado en el constructor.
+	 * @return Resultado final del sumatorio.
+	 */
 	public String mostrar() {
 		String numero = valor_inicial;
         if (numero.length() == 1) {
